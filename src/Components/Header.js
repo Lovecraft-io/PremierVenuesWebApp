af09f003f10d3e991c3e28ec3ee0b83724c685c7
Liam Ellis <liamhellis@gmail.com>
2018-02-27 15:55:40 -0500

    header and nav

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,21 +5,33 @@ import { Menu, Button } from 'semantic-ui-react'
 export const Header = props => {
   console.log(props)
   return (
-    <Menu inverted>
-      <Menu.Item>
-        <h1>Premier Venues</h1>
-      </Menu.Item>
-      <Menu.Menu position="right">
-        {props.links.map(link => (
-          <Menu.Item as={() => <Link to={link.toLowerCase()}>{link}</Link>} />
-        ))}
+    <header id="site-header">
+      <Menu inverted id="site-nav">
         <Menu.Item>
-          <Button>Sign Up</Button>
+          <h1>Premier Venues</h1>
         </Menu.Item>
-        <Menu.Item>
-          <Button>Login</Button>
-        </Menu.Item>
-      </Menu.Menu>
-    </Menu>
+        <Menu.Menu position="right">
+          {props.links.map(link => (
+            <Menu.Item
+              as={() => (
+                <li>
+                  <Link to={link.toLowerCase()}>{link}</Link>
+                </li>
+              )}
+            />
+          ))}
+          <Menu.Item>
+            <Button>
+              <Link to="/signup">Sign Up</Link>
+            </Button>
+          </Menu.Item>
+          <Menu.Item>
+            <Button>
+              <Link to="/login">Login</Link>
+            </Button>
+          </Menu.Item>
+        </Menu.Menu>
+      </Menu>
+    </header>
   )
 }
