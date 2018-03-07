19180eb90f0bcf6032938a38cefc40877107d2c0
Liam Ellis <liamhellis@gmail.com>
2018-03-07 17:21:02 -0500

    venues

--- src/Containers/Pages/Venues/Venues.js
+++ src/Containers/Pages/Venues/Venues.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Segment } from 'semantic-ui-react'
+import {Link} from 'react-router-dom'
+import ReactMarkdown from 'react-markdown'
 // import './venues.css'
 export default class Venues extends Component {
   render() {
     console.log(this.props)
+    const { venues } = this.props
     return (
       <Grid celled="internally">
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Image src="/assets/images/wireframe/image.png" />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Image src="/assets/images/wireframe/centered-paragraph.png" />
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Image src="/assets/images/wireframe/image.png" />
-          </Grid.Column>
-        </Grid.Row>
-
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <Image src="/assets/images/wireframe/image.png" />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Image src="/assets/images/wireframe/paragraph.png" />
-          </Grid.Column>
-          <Grid.Column width={3}>
-            <Image src="/assets/images/wireframe/image.png" />
-          </Grid.Column>
-        </Grid.Row>
+        {this.props.venues.map(venue => (
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Image
+                src={venue.venueMedia ? venue.venueMedia[0].fields.file.url : null}
+              />
+            </Grid.Column>
+            <Grid.Column width={10}>
+              <Segment>
+                <h1><Link to={'venues/' + venue.venueTitle}>{venue.venueTitle}</Link></h1>
+                <ReactMarkdown source={venue.venueBlueprintDetails} />
+                <ReactMarkdown source={venue.venueDescription} />
+              </Segment>
+              <Segment>
+              
+              </Segment>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Image
+                src={venue.venueMedia ? venue.venueMedia[1].fields.file.url : null}
+              />
+            </Grid.Column>
+          </Grid.Row>
+        ))}
       </Grid>
     )
   }
